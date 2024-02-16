5d1375109607866cc00949b814f89f488f2402b4
Amil P A <amilpa2020@gmail.com>
2024-02-17 04:17:49 +0530

    predict

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
-const app = require('express')();
-require('dotenv').config();
+const app = require("express")();
+require("dotenv").config();
+
+const axios = require("axios");
 
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-    res.send('Server running !');
-})
+app.get("/", async (req, res) => {
+  res.send("Success");
+});
 
-app.listen(3000, () => {
-    console.log(`Server running on port ${port}`);
-})
+app.get("/predict", async (req, res) => {
+  const data = req.body;
+
+  const response = await axios.post("http://192.168.220.81:5001/predict", {
+    N: "87",
+    P: "54",
+    K: "20",
+    temperature: "25",
+    humidity: "63",
+    ph: "6",
+    rainfall: "100",
+  });
+
+  res.send(response.data);
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
